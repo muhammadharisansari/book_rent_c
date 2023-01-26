420064281799904822422e7f265e92195e23e758
muhammadharisansari <harisansari08@gmail.com>
2023-01-26 20:43:26 +0800

    bug fixed book list

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -31,25 +31,6 @@ class PublicController extends Controller
         }else {
             $books['books'] = Book::all();
         }
-
-
-        // if ($request->category || $request->title) {
-            //hanya berdasar title
-            // $books['books'] = Book::where('title','like','%'.$request->title.'%')->get();
-            
-            // hanya berdasar category
-            // $books['books'] = Book::whereHas('categories', function($q) use($request){
-            //    $q->where('categories.id',$request->category);
-            // })->get();
-
-            // berdasar satu atau keduanya (categories adalah nama function relasi di model book)
-            // namun pencarian hanya dengan category terjadi bug (tidak terfilter)
-            // $books['books'] = Book::where('title','like','%'.$request->title.'%')->orWhereHas('categories', function($q) use($request) {
-            //     $q->where('categories.id',$request->category);
-            // })->get();
-        // } else {
-        //     $books['books'] = Book::all();
-        // }
         
         $cat['cat'] = Category::all();
         return view('book-list',$books,$cat);
